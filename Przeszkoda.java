5159c73989f516d43298540f0d07d120343319ba
Sp4c3DucK <84458278+Sp4c3DucK@users.noreply.github.com>
2021-05-19 16:26:49 +0200

    Create Przeszkoda.java

---
+++ Przeszkoda.java
@@ -0,0 +1,14 @@
+package lasProjekt;
+
+public class Przeszkoda{
+   private Pozycja pozycja;
+  
+   public Pozycja(Pozycja poz){
+        this.pozycja=poz;
+   }
+     
+   public Pozycja GetPozycja(){
+        return pozycja;
+    }
+    
+}
